fa45b3510890ded107535e3bafc6ec58463e102e
jefernandes10 <jefernandes10@gmail.com>
2022-08-30 22:45:59 -0300

    Create String.py

---
+++ String.py
@@ -0,0 +1,11 @@
+frase = "lorem ipsum dolor sit amet, consectetur adipiscing elit. proin nec."
+frase_em_maiuscula = frase.upper()
+print(frase_em_maiuscula)
+
+frase = "lorem ipsum dolor sit amet, consectetur adipiscing elit. proin nec."
+quantidade_letra_i = frase.count("i")
+print(quantidade_letra_i)
+
+frase = "lorem ipsum dolor sit amet, consectetur adipiscing elit. proin nec."
+frase_eh_toda_minuscula = frase.islower()
+print(frase_eh_toda_minuscula)

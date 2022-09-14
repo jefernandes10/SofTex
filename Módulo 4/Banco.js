3fae11af8bcba784e8dd3ae8e8a47ee8ac4d565e
jefernandes10 <jefernandes10@gmail.com>
2022-09-14 09:00:19 -0300

    Módulo 4

---
+++ Módulo 4/Banco.js
@@ -0,0 +1,12 @@
+const sz = require("sequelize");
+const sequelize = new sz("db", "Antônio", "1234", {
+  dialect: "mysql",
+  host: "192.168.0.150", 
+});
+
+module.exports = sequelize;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
